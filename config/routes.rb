c3651b7f7cd7052d64be949b507c73f7043724e0
Michelle Lam <shelly2.lam@gmail.com>
2019-07-30 11:08:17 -0400

    added Users and bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :artist_genres
   resources :artist_likes
   resources :genres
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_29_214600) do
+ActiveRecord::Schema.define(version: 2019_07_30_144951) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -77,6 +77,13 @@ ActiveRecord::Schema.define(version: 2019_07_29_214600) do
     t.string "content"
     t.string "image_url"
     t.string "video_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
