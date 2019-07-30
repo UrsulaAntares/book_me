e1337c567b9ce6f9198c49fd98428e9f7f678364
github username <github email address>
2019-07-30 11:07:24 -0400

    some work on the calendar; rendered on venue show pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_29_181537) do
+ActiveRecord::Schema.define(version: 2019_07_30_135257) do
 
   create_table "artist_genres", force: :cascade do |t|
     t.integer "artist_id"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2019_07_29_181537) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "genres", force: :cascade do |t|
