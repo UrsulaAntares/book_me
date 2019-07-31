650d328bcc9d7c540266d1e14e9dfaf723898f97
github username <github email address>
2019-07-31 17:06:44 -0400

    Added Genre page to display genres and their associated artists

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,19 @@
 class GenresController < ApplicationController
+
+    before_action :find_genre, only: :show
+
+    def index
+        @genres = Genre.all
+    end
+
+    def show
+        @artists = @genre.artists
+    end
+
+    private
+
+    def find_genre
+        @genre = Genre.find(params[:id])
+    end
+
 end
