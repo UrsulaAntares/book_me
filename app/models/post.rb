747195db1052f560a6da561f8bc215f2dee6e387
Michelle Lam <shelly2.lam@gmail.com>
2019-07-31 15:14:55 -0400

    refactored some view pages

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,16 @@
 class Post < ApplicationRecord
     belongs_to :artist
     has_one_attached :media
+
+    def content_type
+        self.media.blob.content_type
+    end
+
+    def video?
+        self.content_type.include?("video")
+    end
+
+    def image?
+        self.content_type.include?("image" || "jpeg")
+    end
 end
